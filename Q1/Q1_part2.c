9f68b85a9d760c4ab24ec74d1e2fc9c8088e300e
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-10-11 20:11:02 +0530

    Threading Q1(2) done

--- Q1/Q1_part2.c
+++ Q1/Q1_part2.c
@@ -9,9 +9,34 @@
 
 int gloVar = 10;
 
+void *childFunc(){   //child function  --> decrememnts
+
+   printf("\nChild Process begins!\n");
+   while(gloVar>=-90){
+         printf("%d ", gloVar);
+         gloVar--;
+   }
+
+   return NULL;
+}
+
 int main(){
 
+   //child thread's ID
+   pthread_t child;
    
+   //to create a child thread
+   pthread_create(&child, NULL, &childFunc, NULL);
+   
+   
+   //ensures that main(parent) thread waits until the parent thread finishes
+   pthread_join(child, NULL);
+
+   printf("\nParent process begins!\n");
+      while(gloVar<=100){
+         printf("%d ", gloVar);
+         gloVar++;
+      }
 
    return 0;
 }
