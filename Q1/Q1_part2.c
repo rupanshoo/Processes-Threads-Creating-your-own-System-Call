06a169fe7207c224b2f7ee67592b66ea423b0ee5
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-10-11 15:52:53 +0530

    Started with threads

--- Q1/Q1_part2.c
+++ Q1/Q1_part2.c
@@ -2,3 +2,16 @@
    Name: Rupanshoo Saxena
    Roll_Number: 2019096 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int gloVar = 10;
+
+int main(){
+
+   
+
+   return 0;
+}
