63ff80cc50d9f3d75d474efb3e3a5945d51716c9
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-10-11 14:40:26 +0530

    Q1 fork part done

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -2,3 +2,37 @@
    Name: Rupanshoo Saxena
    Roll_Number: 2019096 
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int glVar = 10;  //initializing global variable
+
+
+
+int main(){
+
+   int forkVar;
+   forkVar = fork();
+
+   if(forkVar == 0)  //child process  --> decrements
+   {
+      printf("\nChild Process begins!\n");
+      while(glVar>=-90){
+         printf("%d ", glVar);
+         glVar--;
+      }
+   }
+
+   else   //parent process   --> increments
+   {
+      printf("\nParent process begins!\n");
+      while(glVar<=100){
+         printf("%d ", glVar);
+         glVar++;
+      }
+   }
+
+   return 0;
+}
