a493d386993b81f03adf1fa5e29c7d72a50a207a
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-10-28 22:49:30 +0530

    Q2 done

--- Q2/test.c
+++ Q2/test.c
@@ -2,3 +2,19 @@
    Name: Rupanshoo Saxena
    Roll_Number: 2019096 
 */
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#define call 440
+
+int main(int argc, char * argv){
+	long pid;
+	char filename[1000];
+	scanf("%ld", &pid);
+	scanf("%s", filename);
+	
+	long ret = syscall(call,pid,filename);
+	if(ret ==0) printf("works");
+	else printf("doesnot work");
+	return ret;
+}
