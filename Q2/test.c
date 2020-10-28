a765f057fccff3da79c80f11913e34fc8266cda7
rupanshoo <rupanshoo19096@iiitd.ac.in>
2020-10-28 23:13:39 +0530

    Q2 done

--- Q2/test.c
+++ Q2/test.c
@@ -10,7 +10,9 @@
 int main(int argc, char * argv){
 	long pid;
 	char filename[1000];
+   printf("Enter PID\n");
 	scanf("%ld", &pid);
+   printf("Enter file Name with .txt extension\n");
 	scanf("%s", filename);
 	
 	long ret = syscall(call,pid,filename);
